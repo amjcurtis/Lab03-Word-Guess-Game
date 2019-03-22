5b1c830313919dc9b94498f53b512bd1fd6f7429
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-22 15:10:24 -0700

    Add working helper methods CreateFile() and ReadFile()

--- Lab03-Word-Guess-Game/Program.cs
+++ Lab03-Word-Guess-Game/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab03_Word_Guess_Game
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            string filePath = "../../../file.txt";
+
+            CreateFile(filePath);
+            ReadFile(filePath);
+            Console.ReadLine();
         }
+
+        // Create file
+        public static void CreateFile(string path)
+        {
+            string[] wordBank = { "cat", "mouse", "giraffe", "monkey", "snake" };
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (string word in wordBank)
+                {
+                    sw.WriteLine(word);
+                }
+            }
+        }
+
+        // Read file
+        public static void ReadFile(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        // TODO AddWord() function
+        // If no file exists, create one
+
+        // REQUIREMENTS
+        // User inputs one letter at a time in guesses
+        // Save all guesses, correct and incorrect, throughout each *session*
+        // Show letters that've been correctly guessed for the displayed word
+        // Mystery word in each new session should come from hard-coded bank of words in external .txt file
+        // User can view, add, and delete words in word bank
+
+        // SPECIFICATIONS
+        // METHODS
+        // Home navigation
+        // View words
+        // Add word
+        // Delete word
+        // Exit game
+        // Play game
+
     }
 }
